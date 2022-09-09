57e413945fbcb1c85b3341972919259c4011a83f
schoe <schoe@student.42seoul.kr>
2022-09-09 16:35:51 +0900

    cpp02_fin

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,12 +6,14 @@ float	vec3_cross(t_vec3 lhs, t_vec3 rhs)
 	return (lhs.x * rhs.y - lhs.y * rhs.x);
 }
 
-t_vec3	vec3_minus(t_vec3 lhs, t_vec3 rhs)
+t_vec3	vec3_minus( Point const a, Point const b)
 {
+	t_vec3 lhs = {a.getFloatxVal(), a.getFloatyVal(), 0};
+	t_vec3 rhs = {b.getFloatxVal(), b.getFloatyVal(), 0};
 	return ((t_vec3){lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z});
 }
 
-bool bsp2(t_vec3 line, t_vec3 cross1, t_vec3 cross2)
+bool crossCheck(t_vec3 line, t_vec3 cross1, t_vec3 cross2)
 {
 	float a = vec3_cross(line, cross1);
 	float b = vec3_cross(line, cross2);
@@ -23,16 +25,27 @@ bool bsp2(t_vec3 line, t_vec3 cross1, t_vec3 cross2)
 
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-	t_vec3 line;
-	t_vec3 cross1;
-	t_vec3 cross2;
+	t_vec3 line[3];
+	t_vec3 cross1[3];
+	t_vec3 cross2[3];
 
-	line = vec3_minus((t_vec3){b.getFloatxVal(), b.getFloatyVal(), 0}, (t_vec3){a.getFloatxVal(), a.getFloatyVal(), 0});
-	cross1 = vec3_minus((t_vec3){c.getFloatxVal(), c.getFloatyVal(), 0}, (t_vec3){a.getFloatxVal(), a.getFloatyVal(), 0});
-	cross2 = vec3_minus((t_vec3){point.getFloatxVal(), point.getFloatyVal(), 0}, (t_vec3){a.getFloatxVal(), a.getFloatyVal(), 0});
-	if (bsp2(line, cross1, cross2))
-		std::cout << "true"<< std::endl;
-	else
-		std::cout << "false" << std::endl;
+	line[0] = vec3_minus(b,a);
+	cross1[0] = vec3_minus(c,a);
+	cross2[0] = vec3_minus(point,a);
+	line[1] = vec3_minus(c,b);
+	cross1[1] = vec3_minus(a,b);
+	cross2[1] = vec3_minus(point,b);
+	line[2] = vec3_minus(a,c);
+	cross1[2] = vec3_minus(b,c);
+	cross2[2] = vec3_minus(point,c);
+	for (int i = 0; i < 3; i++)
+	{
+		if (!crossCheck(line[i], cross1[i], cross2[i]))
+		{
+			std::cout << "false" << std::endl;
+			return (false);
+		}
+	}
+	std::cout << "true"<< std::endl;
 	return (true);
 }
--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -4,15 +4,11 @@
 
 int main(void) 
 {
-	Point const a(1,2);
+	Point a(1,2);
 	Point b(0, 0);
 	Point c(2, 0);
-	Point point(0,2);
-
-/*	std::cout << a.getFloatxVal() << "y :" << a.getFloatyVal() << std::endl;
-	std::cout << b.getFloatxVal() << "y :" << b.getFloatyVal() << std::endl;
-	std::cout << c.getFloatxVal() << "y :" << c.getFloatyVal() << std::endl;
-	std::cout << point.getFloatxVal() << "y :" << point.getFloatyVal() << std::endl;*/
+	Point point(1, 1.998);
+	
 	bsp(a, b, c, point);
 	return 0;
 }
